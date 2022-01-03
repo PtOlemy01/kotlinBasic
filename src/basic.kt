2d072675a54026fa39509b7078bdacba6fd15cc3
ptolemy <shapewki1@gmail.com>
2022-01-03 15:40:21 +0900

    String templates

--- src/basic.kt
+++ src/basic.kt
@@ -1,3 +1,10 @@
-/* The comment starts here
-/* contains a nested comment */
- and ends here */
+fun main (){
+    var a = 1
+    // simple name in template:
+    val s1 = "a is $a"
+
+    a = 2
+    // arbitrary expression in template:
+    val s2 = "${s1.replace("is","was")}, but now is $a"
+    println(s2)
+}
