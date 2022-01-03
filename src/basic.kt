67b99708d4849b7c3505a073b840ecff5c6ff47d
ptolemy <shapewki1@gmail.com>
2022-01-03 15:21:29 +0900

    Variables that can be reassigned use the var keyword.

--- src/basic.kt
+++ src/basic.kt
@@ -1,7 +1,5 @@
 fun main(){
-    val a: Int = 1  // immediate assignment
-    val b = 2       // 'Int' type is inferred
-    val c: Int      // Type required when no initializer is provided
-    c = 3           // deferred assignment
+    var x = 5  // 'Int' type is inferred
+    x += 1
 }
 
