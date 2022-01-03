0a5450af020225fcff9cd63412b435b89d92d596
ptolemy <shapewki1@gmail.com>
2022-01-03 16:23:44 +0900

    Conditional expressions

--- src/basic.kt
+++ src/basic.kt
@@ -1,10 +1,11 @@
-fun main (){
-    var a = 1
-    // simple name in template:
-    val s1 = "a is $a"
+fun maxOf(a: Int, b: Int): Int{
+    if ( a > b ){
+        return a
+    } else {
+        return b
+    }
+}
 
-    a = 2
-    // arbitrary expression in template:
-    val s2 = "${s1.replace("is","was")}, but now is $a"
-    println(s2)
+fun main(){
+    println("max of 0 and 42 is ${maxOf(0, 42)}")
 }
