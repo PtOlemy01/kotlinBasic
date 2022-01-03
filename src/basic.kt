63b5a5ed639e9d5398d7dc6166d750aead65b01a
ptolemy <shapewki1@gmail.com>
2022-01-03 16:37:01 +0900

    for loop or ..

--- src/basic.kt
+++ src/basic.kt
@@ -1,6 +1,6 @@
 fun main(){
     val items = listOf("apple", "banana", "kiwifruit")
-    for(item in items){
-        println(item)
+    for(index in items.indices){
+        println("item at $index is ${items[index]}")
     }
 }
