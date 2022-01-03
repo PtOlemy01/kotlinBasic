2b2f6571972d556660ac45db0194b424c1eb284d
ptolemy <shapewki1@gmail.com>
2022-01-03 16:26:54 +0900

    In Kotlin, if can also be used as an expression.

--- src/basic.kt
+++ src/basic.kt
@@ -1,10 +1,4 @@
-fun maxOf(a: Int, b: Int): Int{
-    if ( a > b ){
-        return a
-    } else {
-        return b
-    }
-}
+fun maxOf(a: Int, b: Int) = if( a > b ) a else b
 
 fun main(){
     println("max of 0 and 42 is ${maxOf(0, 42)}")
