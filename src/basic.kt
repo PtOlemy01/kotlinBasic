4ad96d1260c88e6859ce006bf93ee0fa6eec97b5
ptolemy <shapewki1@gmail.com>
2022-01-03 16:30:12 +0900

    for loop

--- src/basic.kt
+++ src/basic.kt
@@ -1,5 +1,6 @@
-fun maxOf(a: Int, b: Int) = if( a > b ) a else b
-
 fun main(){
-    println("max of 0 and 42 is ${maxOf(0, 42)}")
+    val items = listOf("apple", "banana", "kiwifruit")
+    for(item in items){
+        println(item)
+    }
 }
